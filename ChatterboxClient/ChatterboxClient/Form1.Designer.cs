ebd31cc6274e2aa590bd0a3adee020958f2dbe59
TEBco01 <TEBcoyo1@gmail.com>
2015-12-28 15:01:15 -0600

    Added a UI and basic functionallity

--- ChatterboxClient/ChatterboxClient/Form1.Designer.cs
+++ ChatterboxClient/ChatterboxClient/Form1.Designer.cs
@@ -33,6 +33,9 @@
             this.menuItem1 = new System.Windows.Forms.MenuItem();
             this.menuItem2 = new System.Windows.Forms.MenuItem();
             this.menuItem3 = new System.Windows.Forms.MenuItem();
+            this.richTextBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainMenu1
@@ -57,17 +60,53 @@
             this.menuItem3.Index = 2;
             this.menuItem3.Text = "Help";
             // 
+            // richTextBoxOutput
+            // 
+            this.richTextBoxOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBoxOutput.Location = new System.Drawing.Point(12, 12);
+            this.richTextBoxOutput.Name = "richTextBoxOutput";
+            this.richTextBoxOutput.ReadOnly = true;
+            this.richTextBoxOutput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.richTextBoxOutput.Size = new System.Drawing.Size(472, 552);
+            this.richTextBoxOutput.TabIndex = 0;
+            this.richTextBoxOutput.Text = "";
+            this.richTextBoxOutput.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBoxOutput_LinkClicked);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(12, 570);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(391, 20);
+            this.textBoxMessage.TabIndex = 1;
+            this.textBoxMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxMessage_KeyDown);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(409, 570);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 2;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
             // ChatterboxMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(496, 602);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.richTextBoxOutput);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Menu = this.mainMenu1;
             this.Name = "ChatterboxMain";
             this.Text = "Chatterbox";
+            this.Load += new System.EventHandler(this.ChatterboxMain_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ChatterboxMain_KeyDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,6 +116,9 @@
         private System.Windows.Forms.MenuItem menuItem1;
         private System.Windows.Forms.MenuItem menuItem2;
         private System.Windows.Forms.MenuItem menuItem3;
+        private System.Windows.Forms.RichTextBox richTextBoxOutput;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button buttonSend;
     }
 }
 
--- ChatterboxClient/ChatterboxClient/Form1.cs
+++ ChatterboxClient/ChatterboxClient/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,57 @@ namespace ChatterboxClient
         public ChatterboxMain()
         {
             InitializeComponent();
+        }
+
+        //Opens clicked links in the user's default web browser
+        private void richTextBoxOutput_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            Process.Start(e.LinkText);
+        }
+
+        //Activated when the send button is clicked
+        //Calls the function to send the message and clears the textBox
+        //Selects the text box
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            SendMessage(textBoxMessage.Text);
+            textBoxMessage.Text = null;
+            textBoxMessage.Select();
+        }
+
+        //Adds the message to the RichTextBox and moves it to the next line
+        private void SendMessage(string message)
+        {
+            richTextBoxOutput.AppendText(message + "\n");
+        }
+
+        //Called when a key is pressed if the form is selected
+        //Automatically clicks the Send Button if Enter is the key pressed
+        private void ChatterboxMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buttonSend.PerformClick();
+            }
+            
+        }
+
+        //Called when the form loads
+        //Selects the text box
+        private void ChatterboxMain_Load(object sender, EventArgs e)
+        {
+            textBoxMessage.Select();
+        }
+
+
+        //Called when a key is pressed if the text box is selected
+        //Automatically clicks the Send Button if Enter is the key pressed
+        private void textBoxMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buttonSend.PerformClick();
+            }
         }
     }
 }
